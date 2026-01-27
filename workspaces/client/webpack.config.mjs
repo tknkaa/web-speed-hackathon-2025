bc4aef88f441eec07fc96fd0a73cedef4c94e121
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-01-27 15:35:34 +0800

    chore(client): add webpack-bundle-analyzer and update dependencies

--- workspaces/client/webpack.config.mjs
+++ workspaces/client/webpack.config.mjs
@@ -1,6 +1,8 @@
 import path from 'node:path';
 
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import webpack from 'webpack';
+
 
 /** @type {import('webpack').Configuration} */
 const config = {
@@ -61,6 +63,7 @@ const config = {
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({ maxChunks: 1 }),
     new webpack.EnvironmentPlugin({ API_BASE_URL: '/api', NODE_ENV: '' }),
+    new BundleAnalyzerPlugin()
   ],
   resolve: {
     alias: {
